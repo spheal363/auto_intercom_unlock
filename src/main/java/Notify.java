b50415ade3ed744d0de5ffb3f801b1c2ba93fd33
spheal363 <smr.0324@outlook.jp>
2024-12-14 19:15:10 +0900

    imp: 不要なインスタンス化を削除

--- src/main/java/Notify.java
+++ src/main/java/Notify.java
@@ -1,28 +1,24 @@
+import java.io.IOException;
+
 public class Notify {
     public static void main(String[] args) {
         try {
             // トークン読み込み
-            TokenLoader tokenLoader = new TokenLoader("resources/config.properties");
-            String token = tokenLoader.loadToken();
+            String token = TokenLoader.loadToken("resources/config.properties");
 
-            // メッセージ作成
-            String jsonMessage = """
-                    {
-                      "messages": [
-                        {
-                          "type": "text",
-                          "text": "Hello, this is a test message from Java!"
-                        }
-                      ]
-                    }""";
+            // インターホンが押されたときのメッセージ作成
+            String jsonMessage = MessageCreate.LINEMessageCreate("IntercomNotify");
 
-            // 送信処理
+            // ブロードキャストで一斉送信
             LineBroadcaster broadcaster = new LineBroadcaster();
             broadcaster.sendBroadcastMessage(token, jsonMessage);
 
             System.out.println("LINEに通知を送信しました。");
+
+        } catch (IOException e) {
+            System.err.println("ファイルの読み込み中にエラーが発生: " + e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("予期せぬエラーが発生: " + e.getMessage());
         }
     }
 }
--- src/main/java/TokenLoader.java
+++ src/main/java/TokenLoader.java
@@ -4,13 +4,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class TokenLoader {
-    private final String filePath;
-
-    public TokenLoader(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public String loadToken() throws IOException {
+    public static String loadToken(String filePath) throws IOException {
         Properties properties = new Properties();
         try (InputStream input = new FileInputStream(filePath)) {
             properties.load(input);
